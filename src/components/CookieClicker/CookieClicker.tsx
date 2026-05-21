97ae375b886338e9d733b5e4fe5c311603e37785
NehGuk <nurture.tumulus_0n@icloud.com>
2026-05-21 13:39:44 +0200

    Implements CookieClicker point functionality

--- src/components/CookieClicker/CookieClicker.tsx
+++ src/components/CookieClicker/CookieClicker.tsx
@@ -1,14 +1,18 @@
 import "./CookieClicker.css"
 import cookie from "../../../public/cookie.png"
+import { useState } from "react"
 
 export default function CookieClicker() {
+  const [points, setPoints] = useState(0)
+  const handleClick = () => {
+    setPoints(prev => prev + 1)
+  }
 
-  
   return (
     <section>
     <h2>Cookie Clicker</h2>
-    <p>blablabla</p>
-    <button><img className="cookie" src={cookie} /></button>
+    <button className="cookie-btn" onClick={handleClick}><img className="cookie" src={cookie} /></button>
+    <p>Points: {points}</p>
     </section>
   )
 }
