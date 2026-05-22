b6f42d13336476dbad208dce7ed0d9ab49047d4b
NehGuk <nurture.tumulus_0n@icloud.com>
2026-05-22 13:20:03 +0200

    Adds table showing users from mockData

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,8 +1,30 @@
+import { mockData } from "../../data/mockData"
+import type { User } from "../../data/mockData"
+import { useState } from "react"
+
 export default function Users() {
+  const [users, setUsers] = useState<User[] | null>(mockData)
   return (
     <section>
       <h2>Users</h2>
-      <p>blablabla</p>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users?.map((user, i) => {
+            return (
+              <tr key={i}>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
     </section>
   )
 }
---
+++ src/data/mockData.ts
@@ -0,0 +1,11 @@
+export type User = {
+  username: string
+  email: string
+}
+
+export const mockData: User[] = [
+  { username: "Ola Normann", email: "ola.normann@norge.no" },
+  { username: "Torleif", email: "torleif@kodehode.no" },
+  { username: "Jan Egil", email: "jan.egil@kodehode.no" },
+  { username: "Sander", email: "sander@kodehode.no" },
+]
